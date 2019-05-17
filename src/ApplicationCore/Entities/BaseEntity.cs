e19f4e6b9c4848b0051baee6cbaee4149783a6fb
Malhaar Punjabi <mpunjabi@vacc.com.au>
2019-05-17 16:26:20 +1000

    repository specfication added.

---
+++ src/ApplicationCore/Entities/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationCore.Entities
+{
+    public class BaseEntity
+    {
+        public Guid ID { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateUpdated { get; set; }
+    }
+}
---
+++ src/Web/Controllers/BaseApiController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class BaseApiController : Controller
+    { }
+}
