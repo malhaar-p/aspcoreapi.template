abc9253117f470087c7a62da10e75b5897e0d814
Malhaar <malhaar.p@outlook.com>
2019-05-26 21:54:22 +1000

    sample work 1

---
+++ src/ApplicationCore/Entities/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationCore.Entities
+{
+    public class Category : BaseEntity
+    {
+        public string Name { get; set; }
+
+
+        // relatives
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
---
+++ src/ApplicationCore/Entities/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationCore.Entities
+{
+    public class Product : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public byte[] Image { get; set; }
+        public string ImagesUrl { get; set; }
+
+        // more fields related to prodct size, dimension, etc.
+
+
+        // relatives
+        public Guid CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+
+    }
+}
